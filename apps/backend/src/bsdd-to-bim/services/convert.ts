6e3e101f878ea743a5a945259331e1ce7e21676f
Robert Bornschein <robertbornscheinofficial@gmail.com>
2025-09-22 19:19:14 +0200

    use swagger

--- apps/backend/src/bsdd-to-bim/services/convert.ts
+++ apps/backend/src/bsdd-to-bim/services/convert.ts
@@ -1,11 +1,12 @@
 import type {BSDDDictionaryMetadata} from "../types/bsdd-dictionary.types.js";
 import { xmlBuilder } from "../utils/xml-builder.utils.js";
+import type {DictionaryPropertiesResponseContractV1} from "../types/swagger.types.js";
 
-export const convert = (json: BSDDDictionaryMetadata): string => {
-	const myInput = json.Properties?.map(bsddProperty => {
+export const convert = (propertiesOnDictionary: DictionaryPropertiesResponseContractV1): string => {
+	const myInput = propertiesOnDictionary?.properties?.map(bsddProperty => {
 		return {
-			[bsddProperty.Name]: {
-				"code": bsddProperty.Code,
+			[bsddProperty.name!]: {
+				"code": bsddProperty.code,
 			}
 		}
 	})
