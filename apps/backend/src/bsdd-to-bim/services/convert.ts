9289b76f1a9871de1c0ca18f361e80f7d0231741
Robert Bornschein <robertbornscheinofficial@gmail.com>
2025-09-22 19:14:33 +0200

    swagger types & vitest

--- apps/backend/src/bsdd-to-bim/services/convert.ts
+++ apps/backend/src/bsdd-to-bim/services/convert.ts
@@ -1,7 +1,20 @@
-import type { BSDDDictionaryMetadata } from "../types/bsdd-dictionary.types.js";
+import type {BSDDDictionaryMetadata} from "../types/bsdd-dictionary.types.js";
 import { xmlBuilder } from "../utils/xml-builder.utils.js";
 
 export const convert = (json: BSDDDictionaryMetadata): string => {
-	const myInput = json;
+	const myInput = json.Properties?.map(bsddProperty => {
+		return {
+			[bsddProperty.Name]: {
+				"code": bsddProperty.Code,
+			}
+		}
+	})
 	return xmlBuilder.build(myInput);
 };
+
+/**
+<PropertyName>
+	<code>propery-code</code>
+	</PropertyName>
+
+ */
